3a9909807070f38b47b115eeaef4665a501151b6
harshitcorp09 <162092528+harshitcorp09@users.noreply.github.com>
2025-09-25 15:41:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,14 +7,18 @@ terraform {
   }
 }
 
+provider "tfe" {
+  # You can omit this block in TFE VCS-driven workspaces
+}
+
 resource "tfe_registry_module" "registry-catalog" {
   name         = "sample-module"
-  organization = "Harshit"
-  provider     = "aws"
+  organization = "Harshit" # Make sure this matches your actual TFE org name
+  provider     = "aws"      # This is correct as a string here
 
   vcs_repo {
     identifier         = "harshitcorp09/terraform-aws-ec2-instance"
-    display_identifier = "harshitcorp09/terraform-aws-ec2-instance" # This is the field that may cause drift
+    display_identifier = "harshitcorp09/terraform-aws-ec2-instance"
     branch             = "main"
   }
 }
